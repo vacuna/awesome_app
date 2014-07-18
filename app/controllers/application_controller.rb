eb50abdfcb43988cb3f676c79559226ceba3da19
Your Name <you@example.com>
2014-07-18 19:30:46 +0000

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ class ApplicationController < ActionController::Base
   def show
     @user = User.find_by_id(params['id'])
   end
+  
+  def new
+  end
+  
+ def create
+   g = User.new
+   g.name = params['name'] 
+   g.url = params['url']
+   g.save
+   redirect_to "/user/#{ g.id }"
+ end
+  
+  def edit
+    @user = User.find_by_id(params['id'])
+  end
+  
+  def update
+    g = User.find_by_id(params['id'])
+    g.name = params['name'] 
+    g.url = params['url']
+    g.save
+    redirect_to "/user/#{ g.id }"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/users/:id' => 'application#show'
+  get '/user/:id' => 'application#show'
+  get '/new_user' => 'application#new'
+  get '/create_user' => 'application#create'
+  get '/user/:id/edit' => 'application#edit'
+  get '/update_user/:id' => 'application#update'
 end
